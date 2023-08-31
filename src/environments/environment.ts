18f19c7c52547c2d997cc55f1284246ecda2238f
Alonso Novoa <aisaacnsoto@gmail.com>
2023-08-31 09:25:37 -0500

    ajustes para producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    PAYPAL_CLIENT_ID: '',
-    BACKEND_URL: ''
+    PAYPAL_CLIENT_ID: 'AfNvzVu8QpQm_567j61meQ8aLyPlCgXjwt5p-7QLwOwjl4_vRhSgUvky7dZNjRPX4R79d7SQfsxiVvea',
+    BACKEND_URL: 'https://tienda-virtual-mean-stack-backend.onrender.com'
 };
